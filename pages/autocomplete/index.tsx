0271bdbb8415208669b76a303ffaab8ca9ca682b
hvi <hvi@lesfurets.com>
2021-11-28 05:43:28 +0100

    Fork a Slate implementation

--- pages/autocomplete/index.tsx
+++ pages/autocomplete/index.tsx
@@ -1,40 +1,192 @@
-import { useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { createEditor, Descendant, Editor, Range, Transforms } from "slate";
+import { withHistory } from "slate-history";
+import {
+  Editable,
+  Slate,
+  useFocused,
+  useSelected,
+  withReact,
+} from "slate-react";
+import data from "../../components/data.json";
 import styles from "./index.module.css";
-import { TextArea } from "../../components/TextArea";
-import { UserAutoCompleteRadio } from "../../components/UserAutoCompleteRadio";
 
-const AutoCompletePage = () => {
-  const [value, setValue] = useState("");
-  const [displayAutoComplete, setDisplayAutoComplete] = useState(false);
+const DiscordTextArea = () => {
+  const ref = useRef<HTMLDivElement | null>();
+  const [value, setValue] = useState<Descendant[]>(initialValue);
+  const [target, setTarget] = useState<Range | undefined>();
+  const [index, setIndex] = useState(0);
+  const [search, setSearch] = useState("");
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const editor = useMemo(
+    () => withMentions(withReact(withHistory(createEditor()))),
+    []
+  );
 
-  const onChange = (e) => {
-    if (
-      (displayAutoComplete && e.target.value.endsWith(" ")) ||
-      e.target.value === ""
-    ) {
-      setDisplayAutoComplete(false);
-    }
-    if (e.target.value.endsWith(" @") || e.target.value === "@") {
-      setDisplayAutoComplete(true);
-    }
-    setValue(e.target.value);
-  };
+  const users = data.users
+    .map((user) => user.username)
+    .filter((c) => c.toLowerCase().startsWith(search.toLowerCase()))
+    .slice(0, 10);
 
-  const onSubmit = (user) => {
-    setValue(value + user.username);
-    setDisplayAutoComplete(false);
-  };
+  const onKeyDown = useCallback(
+    (event) => {
+      if (target) {
+        switch (event.key) {
+          case "ArrowDown":
+            event.preventDefault();
+            const prevIndex = index >= users.length - 1 ? 0 : index + 1;
+            setIndex(prevIndex);
+            break;
+          case "ArrowUp":
+            event.preventDefault();
+            const nextIndex = index <= 0 ? users.length - 1 : index - 1;
+            setIndex(nextIndex);
+            break;
+          case "Tab":
+          case "Enter":
+            event.preventDefault();
+            Transforms.select(editor, target);
+            insertMention(editor, users[index]);
+            setTarget(null);
+            break;
+          case "Escape":
+            event.preventDefault();
+            setTarget(null);
+            break;
+        }
+      }
+    },
+    [index, search, target]
+  );
 
   return (
-    <section className={styles.textSection}>
-      {displayAutoComplete && <UserAutoCompleteRadio onSubmit={onSubmit} />}
-      <TextArea value={toDisplayedValue(value)} onChange={onChange} />
-    </section>
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(value) => {
+        setValue(value);
+        const { selection } = editor;
+
+        if (selection && Range.isCollapsed(selection)) {
+          const [start] = Range.edges(selection);
+          const wordBefore = Editor.before(editor, start, { unit: "word" });
+          const before = wordBefore && Editor.before(editor, wordBefore);
+          const beforeRange = before && Editor.range(editor, before, start);
+          const beforeText = beforeRange && Editor.string(editor, beforeRange);
+          const beforeMatch = beforeText && beforeText.match(/^@(\w+)$/);
+          const after = Editor.after(editor, start);
+          const afterRange = Editor.range(editor, start, after);
+          const afterText = Editor.string(editor, afterRange);
+          const afterMatch = afterText.match(/^(\s|$)/);
+
+          if (beforeMatch && afterMatch) {
+            setTarget(beforeRange);
+            setSearch(beforeMatch[1]);
+            setIndex(0);
+            return;
+          }
+        }
+
+        setTarget(null);
+      }}
+    >
+      {target && users.length > 0 && (
+        <div>
+          <div>
+            {users.map((char, i) => (
+              <div
+                key={char}
+                style={{
+                  padding: "1px 3px",
+                  borderRadius: "3px",
+                  background: i === index ? "#B4D5FF" : "transparent",
+                }}
+              >
+                {char}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      <section className={styles.textSection}>
+        <Editable
+          renderElement={renderElement}
+          onKeyDown={onKeyDown}
+          placeholder="Envoyer un message dans #welcome"
+        />
+      </section>
+    </Slate>
   );
 };
 
-const toDisplayedValue = (value) => {
-  return value;
+const withMentions = (editor) => {
+  const { isInline, isVoid } = editor;
+
+  editor.isInline = (element) => {
+    return element.type === "mention" ? true : isInline(element);
+  };
+
+  editor.isVoid = (element) => {
+    return element.type === "mention" ? true : isVoid(element);
+  };
+
+  return editor;
 };
 
-export default AutoCompletePage;
+const insertMention = (editor, character) => {
+  const mention = {
+    type: "mention",
+    character,
+    children: [{ text: "" }],
+  };
+  Transforms.insertNodes(editor, mention);
+  Transforms.move(editor);
+};
+
+const Element = (props) => {
+  const { attributes, children, element } = props;
+  switch (element.type) {
+    case "mention":
+      return <Mention {...props} />;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const Mention = ({ attributes, children, element }) => {
+  const selected = useSelected();
+  const focused = useFocused();
+  return (
+    <span
+      {...attributes}
+      contentEditable={false}
+      data-cy={`mention-${element.character.replace(" ", "-")}`}
+      style={{
+        padding: "3px 3px 2px",
+        margin: "0 1px",
+        verticalAlign: "baseline",
+        display: "inline-block",
+        borderRadius: "4px",
+        backgroundColor: "#eee",
+        fontSize: "0.9em",
+        boxShadow: selected && focused ? "0 0 0 2px #B4D5FF" : "none",
+      }}
+    >
+      @{element.character}
+      {children}
+    </span>
+  );
+};
+
+const initialValue: Descendant[] = [
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: "",
+      },
+    ],
+  },
+];
+
+export default DiscordTextArea;
